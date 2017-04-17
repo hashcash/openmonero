27377ebc40a01d53d0620aad2e5e7ce30da83f47
moneroexamples <moneroexamples@tuta.io>
2017-04-17 16:42:05 +0800

    ProjectMonero.cmake build release build

--- cmake/ProjectMonero.cmake
+++ cmake/ProjectMonero.cmake
@@ -7,9 +7,13 @@ ExternalProject_Add(monero
         DOWNLOAD_NO_PROGRESS 0
         URL https://github.com/monero-project/monero/archive/v0.10.3.1.tar.gz
         BUILD_IN_SOURCE 1
+        CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release
         BUILD_COMMAND make
         LOG_BUILD 0
         INSTALL_COMMAND "")
 
-set(MONERO_BUILD_DIR ${CMAKE_SOURCE_DIR}/deps/src/monero CACHE PATH "The path to the Monero build directory")
-set(MONERO_SOURCE_DIR ${CMAKE_SOURCE_DIR}/deps/src/monero CACHE PATH "The path to the Monero source directory")
+set(MONERO_BUILD_DIR ${CMAKE_SOURCE_DIR}/deps/src/monero
+        CACHE PATH "The path to the Monero build directory")
+
+set(MONERO_SOURCE_DIR ${CMAKE_SOURCE_DIR}/deps/src/monero
+        CACHE PATH "The path to the Monero source directory")
